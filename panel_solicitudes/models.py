a9a1e597eed625c2d9484835dc1d067251783ebd
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2021-10-12 00:06:07 -0300

    Cambios en models.py Tablas Fotos, Solicitud y Usuario

--- panel_solicitudes/models.py
+++ panel_solicitudes/models.py
@@ -51,11 +51,12 @@ class Usuario(models.Model):
     fecha_nacimiento = models.DateField()
     direccion = models.TextField()
     genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+    verificacion = models.ForeignKey(Verificacion, on_delete=models.CASCADE)
     created_date = models.DateTimeField(
         default=timezone.now)
 
     def __str__(self):
-        return "%s %s %s" % (self.rut, self.nombre, self.apellidos)
+        return "%s %s %s %s" % (self.rut, self.nombre, self.apellidos, self.verificacion)
 
     #
     #
@@ -66,7 +67,8 @@ class Fotos(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, blank=True, null=True)
     upload = models.FileField(upload_to=user_directory_path)
     def __str__(self):
-        return self.usuario.rut
+        return "%s %s %s" % (self.usuario.nombre, self.usuario.apellidos, self.usuario.rut)
+        #return self.usuario.rut
  
 class PerfilMaestro(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, blank=True, null=True)
@@ -86,6 +88,6 @@ class SolicitudTarea(models.Model):
         default=timezone.now)
     fecha = models.DateTimeField()
     def __str__(self):
-        return "%s %s %s" % (self.nombre, self.tarea, self.direccion)
+        return "%s %s %s %s" % (self.nombre, self.tarea, self.direccion, self.estado)
 
 
