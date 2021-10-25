ffdf667b9b036e7b5c00f63e072b83b8d0693f0d
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2021-10-25 02:36:12 -0300

    Se agrega Templates, se crea la pagina para la Categorias de trabajos, se separa el codigo limpio del codigo extra separando en bloques, se usa CSS basico para comprobar el funcionamiento

--- panel_solicitudes/urls.py
+++ panel_solicitudes/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.maestros_lista, name='maestros_lista'),
+    path('', views.home),
+    path('maestros', views.maestros_lista, name='maestros_lista'),
+    path('categorias', views.categorias_lista, name='categorias_lista'),
+   
 ]
--- panel_solicitudes/views.py
+++ panel_solicitudes/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import *
+from django.utils import timezone
 
 # Create your views here.
+# Pagina de Bienvenida para los usuarios nuevos
+def home(request):
+    return render(request, 'panel_solicitudes/templates/home.html', {} )
+
+
+# Pagina para listar los Maestros
 def maestros_lista(request):
     return render(request, 'panel_solicitudes/templates/maestros_lista.html', {})
+
+
+def categorias_lista(request):
+    categorias = Categoria.objects.order_by('nombre')
+    return render(request, 'panel_solicitudes/templates/categorias_lista.html', {'categorias': categorias} )
+
