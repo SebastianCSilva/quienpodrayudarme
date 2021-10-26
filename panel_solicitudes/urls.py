ead64df480dda5dc3951fb8f8498ede31604ea66
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2021-10-26 03:36:35 -0300

    Se agrega el iniciar sesion dentro de la aplicacion y el cerrar la sesion con las redirecciones correspondientes

--- panel_solicitudes/urls.py
+++ panel_solicitudes/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name=''),
+    path('home', views.home, name='home'),
     path('maestros', views.maestros_lista, name='maestros_lista'),
     path('categorias', views.categorias_lista, name='categorias_lista'),
     path('registrar', views.registrar, name='registrar'),
-    path('login', views.login, name='login'),
+    path('login', views.login_pagina, name='login'),
+    path('logout', views.logout_usuario, name='logout'),
    
 ]
--- panel_solicitudes/views.py
+++ panel_solicitudes/views.py
@@ -1,12 +1,13 @@
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.utils import timezone
 from .forms import *
-from django.contrib.auth import logout as do_logout
-from django.contrib.auth import authenticate, login as dj_login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 # Pagina de Bienvenida para los usuarios nuevos
@@ -20,13 +21,35 @@ def registrar(request):
         form = UsuarioCreacionForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
 
     context = {'form': form}
 
     return render(request, 'panel_solicitudes/templates/registrar.html', context)
 
-def login(request):
-    return render(request, 'panel_solicitudes/templates/login.html', {} )
+def login_pagina(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Apodo o Contraseña incorrectos')
+            
+    context = {}
+    
+    return render(request, 'panel_solicitudes/templates/login.html', context )
+
+def logout_usuario(request):
+    logout(request)
+    return redirect('login')
+
 
 # Pagina para listar los Maestros
 def maestros_lista(request):
